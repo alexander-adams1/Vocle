3da7e84a7fa366d5e9115faba84e202bd58cd993
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2022-11-28 18:31:44 -0500

    feat: Setup backend boilerplate code

---
+++ backend/src/main/java/Server.java
@@ -0,0 +1,19 @@
+import static spark.Spark.after;
+
+import spark.Spark;
+
+public class Server {
+  public static void main(String[] args) {
+    Spark.port(3232);
+    after(
+        (request, response) -> {
+          response.header("Access-Control-Allow-Origin", "*");
+          response.header("Access-Control-Allow-Methods", "*");
+        });
+    Spark.init();
+    Spark.awaitInitialization();
+    System.out.println("Server started.");
+  }
+}
+
+
