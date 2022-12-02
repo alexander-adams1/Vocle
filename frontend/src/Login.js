319da797fc07645741f5c3240e9635f35054e8d1
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2022-12-02 15:50:16 -0500

    feat: Added onClick boilerplate

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,7 +4,7 @@ function Login() {
     return (
         <div className="App">
             <header className="App-header">
-                <button className="btn-spotify" href="/auth/login" onClick={console.log(1)}>
+                <button className="btn-spotify" href="/auth/login" onClick={() => console.log(1)}>
                     Login with Spotify 
                 </button>
             </header>
