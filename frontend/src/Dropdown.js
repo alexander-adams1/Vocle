0ff2953f87c8fa3afb91ec20f86c2d52696f9fed
Ivan <ivan_sung@brown.edu>
2022-12-08 17:49:09 -0500

    FEAT: added in search bar for multiplayer and mocked API responses

---
+++ frontend/src/Dropdown.js
@@ -0,0 +1,40 @@
+import AsyncSelect from "react-select/async";
+
+const Dropdown = () => {
+    // mocking data taken from backend API request.
+    const mockOptions = [
+        {value: "22 - Taylor Swift", label: "22 - Taylor Swift"}, 
+        {value: "Can't Hold Us (feat. Ray Dalton) - Macklemore & Ryan Lewis, Macklemore, Ryan Lewis, Ray Dalton", label: "Can't Hold Us (feat. Ray Dalton) - Macklemore & Ryan Lewis, Macklemore, Ryan Lewis, Ray Dalton"},
+        {value: "Antidode - Travis Scott", label: "Antidote - Travis Scott"}
+    ]
+
+    const handleChange = (selectedOption) => {
+       console.log("handleChange", selectedOption); 
+    };
+
+    const loadOptions = (searchValue, callback) => {
+        setTimeout(() => {
+            const filteredOptions = mockOptions.filter((option) => 
+            option.label.toLowerCase().includes(searchValue.toLowerCase())
+            );
+            console.log("loadOptions", searchValue, filteredOptions);
+            callback(filteredOptions);
+        }, 2000);
+    };
+
+    const colorStyles = {
+        control: styles => ({...styles, zIndex: 9999}),
+        option: (styles, {isDisabled, isFocused, isSelected}) => {
+            return {
+                ...styles,
+                zIndex: isDisabled ? 0: 9999,
+                cursor: isDisabled ? 'not-allowed' : 'default'
+            };
+        }
+    }
+
+    return <AsyncSelect className="dropdown" placeholder={<div>Know the song? Search for the artist/title</div>} loadOptions={loadOptions} onChange={handleChange}/>
+   
+}
+
+export default Dropdown;
