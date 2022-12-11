53090855117f61d5f495893fe53d9d2f3ce2f326
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2022-12-11 10:48:43 -0500

    feat: Added getting the track URL from backend

--- backend/src/main/java/GenerateTrackHandler.java
+++ backend/src/main/java/GenerateTrackHandler.java
@@ -74,6 +74,7 @@ public class GenerateTrackHandler implements Route {
     int track_number = getRandomInteger(0, TrackDataJSON.getInt("total"));
     this.responseMap.put("Result", "Success");
     this.trackID = TrackDataJSON.getJSONArray("items").getJSONObject(track_number).getJSONObject("track").getString("id");
+    String trackURL = TrackDataJSON.getJSONArray("items").getJSONObject(track_number).getJSONObject("track").getJSONObject("external_urls").getString("spotify");
     ArrayList<String> trackNameAndArtistList = new ArrayList<>();
     for (int i = 0; i < TrackDataJSON.getInt("total"); i++) {
       String trackName = TrackDataJSON.getJSONArray("items").getJSONObject(i).getJSONObject("track").getString("name");
@@ -82,6 +83,7 @@ public class GenerateTrackHandler implements Route {
       trackNameAndArtistList.add(trackName + " - " + artistName);
     }
     this.responseMap.put("Track and Artists List", trackNameAndArtistList.toString());
+    this.responseMap.put("Track URL", trackURL);
   }
 
   private int getRandomInteger(int min, int max) {
---
+++ frontend/src/AudioPlayer.tsx
@@ -0,0 +1,11 @@
+import Sound from 'react-sound';
+
+// export default function PlaySound() {
+//     return (
+//         <div>
+//             <Sound 
+//                 playFromPosition={0}
+//             />
+//         </div>
+//     )
+// }
