8a0c3f23d6cfa74b3f0d1aa7f6eaf2e7681b7fc3
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2022-12-11 14:09:07 -0500

    feat: Added changes to properly generate random songs on the backend

--- frontend/src/AudioPlayer.tsx
+++ frontend/src/AudioPlayer.tsx
@@ -1,11 +1,15 @@
+import { accessToken } from 'mapbox-gl';
 import Sound from 'react-sound';
+import { generateAccessToken, generateTrack } from './GenerateSong';
 
-// export default function PlaySound() {
-//     return (
-//         <div>
-//             <Sound 
-//                 playFromPosition={0}
-//             />
-//         </div>
-//     )
-// }
+var URL = generateTrack(`https://open.spotify.com/playlist/4sX9J6HyFYZxtauo6Xu37o`, accessToken)
+export default function PlaySound() {
+    return (
+        <div>
+            <Sound
+                url={URL.toString()}
+                playFromPosition={0}
+            />
+        </div>
+    )
+}
--- frontend/src/GenerateSong.tsx
+++ frontend/src/GenerateSong.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 
-function generateToken() {
-    return new Promise((resolve) => {
-        fetch('localhost:3232/SpotifyToken')
-        .then(res => res.json())
-        .then(response => {
-            if (response.result === `Success, got an access token`) {
-                resolve(response.access_token)
-            }
-            else {
-                resolve(`An error occured while trying to generate an access token`)
-            }
-        })
-        .catch(e => resolve(`Could not connect to API server`))
+function generateAccessToken() {
+    fetch('localhost:3232/SpotifyToken')
+    .then(res => res.json())
+    .then(response => {
+        if (response.result === `Success, got an access token`) {
+            return response.access_token
+        }
+        else {
+            return `An error occured while trying to generate an access token`
+        }
+    })
+    .catch(e => {
+        return `Could not connect to API server`
     })
 }
 
-function generatePlaylist(playlistURL : String, accessToken: string) {
+function generateTrack(playlistURL : String, accessToken: string) {
     const playlistID = playlistURL.split('/').slice(-1)[0]
-    return new Promise((resolve) => {
-        fetch(`localhost:3232/GenerateTrack?accessToken=${accessToken}&playlistID=${playlistID}`)
-        .then(res => res.json())
-        .then(response => {
-            if (response.result === `Success`) {
-                resolve(response.TrackURL)
-            }
-            else if (response.result === `Error`) {
-                resolve(response.Track)
-            }
-            else {
-                resolve(`An error occured while trying to generate a random song`)
-            }
-        })
-        .catch(e => resolve(`Could not connect to API server`))
+    fetch(`localhost:3232/GenerateTrack?accessToken=${accessToken}&playlistID=${playlistID}`)
+    .then(res => res.json())
+    .then(response => {
+        if (response.result === `Success`) {
+            return response.TrackURL
+        }
+        else if (response.result === `Error`) {
+            return response.Track
+        }
+        else {
+            return `An error occured while trying to generate a random song`
+        }
+    })
+    .catch(e => {
+        return `Could not connect to API server`
     })
 }
+
+export {generateAccessToken, generateTrack}
