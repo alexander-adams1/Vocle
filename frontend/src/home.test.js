66adf3129227b381e85d7cff449fc2f02bb48626
Ivan <ivan_sung@brown.edu>
2022-12-11 16:02:05 -0500

    started frontend homepage test suite

---
+++ frontend/src/home.test.js
@@ -0,0 +1,27 @@
+import * as home from './Home.js';
+
+defaultHTML = `<div className="v1_3">
+<div className="v122_910">
+  <div className="v10_67"></div>
+</div>
+<div className="v122_909">
+  <button id="singleplayer-button" className="v122_912" onClick={this.displayQuestionSingle}><span className="v10_7">Singleplayer
+  </span></button>
+</div>
+
+<div className="v122_913">
+  <div className="v45_3"></div>
+</div>
+<div className="v71_26">
+  <div className="v71_31"></div>
+  <div className="v71_29"></div><span className="v71_28">Vocle</span>
+  <div className="v71_30"></div>
+  <div className="v71_33"></div>
+</div>
+<div className="v122_999">
+  <button id="multiplayer-button" className="v122_915" onClick={this.displayQuestionMulti}><span className="v10_8">Multiplayer
+  </span>
+  </button>
+</div>
+</div>`
+
