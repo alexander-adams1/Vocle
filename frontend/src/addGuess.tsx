e6a11c086ea857ef30ec67566605cf95aec33537
Ayush <ayush_gupta@brown.edu>
2022-12-16 03:08:15 -0500

    Guesses showing up on screen in singleplayer

--- frontend/src/addGuess.tsx
+++ frontend/src/addGuess.tsx
@@ -1,69 +1,88 @@
-import React, {useState,  SetStateAction, Component } from 'react'
+import React, { useState, SetStateAction, Component } from 'react'
 import 'react/jsx-runtime'
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Question from './Questionssingleplayer';
 import Dropdown from './Dropdown';
 import Home from './Home';
 import { render } from '@testing-library/react';
 import { ListComponent } from './listComponent';
+import { stringify } from 'querystring';
 
-function Addsong(){
-    const [service, setService] = useState([{song: ''}])
-    const handleServiceAdd = () => {
-        console.log("song called")
-        let newSong = '';
-        console.log(newSong)
-        const text: Element | null = document.getElementById('dropdown_class')
-        if(text == null) {
-            console.log("No text in the text box")
+function Addsong() {
+  var [service, setService] = useState<{ song: string }[]>([]);
+  const handleServiceAdd = () => {
+    console.log("song called")
+    let newSong = '';
 
-        } else if (!(text instanceof HTMLDivElement)) {
-            console.log(`Found element ${text}, but it wasn't an input`)
+    const text: Element | null = document.getElementById('dropdown_class')
+    if (text == null) {
+      console.log("No text in the text box")
+
+    } else if (!(text instanceof HTMLDivElement)) {
+      console.log(`Found element ${text}, but it wasn't an input`)
+    } else {
+      //this.displayAnswerSingle
+      if (typeof text.textContent === 'string') {
+        newSong = text.textContent
+      }
+      if (newSong !== 'Know the song? Search for the artist/title') {
+        if (service.length < 6) {
+          setService([...service, { song: newSong }]);
         } else {
-            console.log("submit called")
-            console.log(text.textContent)
-            //this.displayAnswerSingle
-            if(text.textContent === 'string')
-            {
-                newSong=text.textContent
-            }
+          service.splice(0, service.length);
+          // TODO: Add method for bringing up the modal and ending the round
         }
-        setService([...service, {song: newSong}]);
-    
-      };
-      const handleNullSongAdd = () => {
-          console.log("called")
-        setService([...service, {song: 'Guess skipped'}]);
-      };
+      }
+    }
 
 
-        return(
-           <> 
-        {service.map((singleService, index) => (
-        <div key={index}className="services"> 
-        <div className="first-division">
-        <div className="v54_92">
-                   <button className="v54_91" role="Submit" onClick={handleServiceAdd} ><span className="v54_90">SUBMIT</span>
-                   </button></div>
+  };
+  const handleNullSongAdd = () => {
+    console.log("called")
+    if (service.length < 6) {
+      setService([...service, { song: 'Guess skipped' }]);
+    } else { 
+      service.splice(0, service.length);
+      // TODO: Add method for bringing up the modal and ending the round
+    }
+  };
+
+  // Code below is for only allowing button to call helper method when text box isn't empty
+
+  // <button className="v54_91" role="Submit" onClick={() => {
+  //   const text: Element | null = document.getElementById('dropdown_class')
+  //   if (text != null && !(text instanceof HTMLDivElement) ) {
+  //     if (text.textContent?.includes('')) {
+  //       handleServiceAdd();
+  //     }
+  //   }
+  // }} >
+
+  return (
+    <>
+        <div className="services">
+          <div className="first-division">
+            <div className="v54_92">
+              <button className="v54_91" role="Submit" onClick={handleServiceAdd} ><span className="v54_90">SUBMIT</span>
+              </button>
+            </div>
             <div className="v54_93"><button className="v54_94" onClick={handleNullSongAdd}>
-                       <span className="v54_95">SKIP</span>
-                   </button></div>
-                   </div></div>))}
+              <span className="v54_95">SKIP</span>
+            </button></div>
+          </div></div>
 
-                   <div className="output">
-        <h2>Output</h2>
-        {service &&
-          service.map((singleService, index) => (
-            <ul key={index}>
-              {singleService.song && <li>{singleService.song}</li>}
-            </ul>
-          ))}
+      <div className="output">
+        {service.map((item, index) => (
+          <ul className="output_list">
+            <li className="output_el" key={index}>{item.song}</li>
+          </ul>
+        ))}
       </div>
-                   
-            </>
-            
-    )
-    
+
+    </>
+
+  )
+
 }
 
 export default Addsong;
