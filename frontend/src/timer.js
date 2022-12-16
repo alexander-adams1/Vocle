a004b636e5fd9a15c3eb2dc922879f7e05f39eea
Ayush <ayush_gupta@brown.edu>
2022-12-15 23:34:22 -0500

    Formatting

--- frontend/src/timer.js
+++ frontend/src/timer.js
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import { use1Second } from "./useSeconds";
 import playAudio from './AudioPlayer';
 
-export const Timer = (
+export const SingleTimer = (
     {
     
   seconds: initialSeconds = 0,
@@ -30,12 +30,48 @@ export const Timer = (
   console.log('timer')
   use1Second(tick);
   return (
-    <div className="timerclass">
-      <div className="multiplayerPlayButton" aria-label="click to play the song"> <div onClick={running ? pause : start}> <div className="v54_101"></div><button className="v54_100"></button></div> 
+    <div className="singletimerclass">
+      <div className="PlayButton" aria-label="click to play the song"> <div onClick={running ? pause : start}> <div className="v54_101"></div><button className="v54_100"></button></div> 
       </div>    
     <div className="greenRectangle"> {seconds} seconds </div>
     </div>
   )
+};
+
+export const MultiTimer = (
+  {
+  
+seconds: initialSeconds = 0,
+running: initiallyRunning = false
+} = {}) => {
+const [seconds, setSeconds] = useState(initialSeconds);
+const [running, setRunning] = useState(initiallyRunning);
+const tick = useCallback(
+  () => (running ? setSeconds((seconds) => seconds + 1) : undefined),
+  [running]
+);
+const start = () => {
+  setRunning(true);
+  playAudio(`https://p.scdn.co/mp3-preview/2e3c2595984f1beef0c621672469359157e98d3c?cid=fbf528e0063e4820b4fd570f750f297d`, 4)
+}
+const pause = () => {
+  setRunning(false);
+  playAudio(`https://p.scdn.co/mp3-preview/2e3c2595984f1beef0c621672469359157e98d3c?cid=fbf528e0063e4820b4fd570f750f297d`, 4)
+}
+const reset = () => setSeconds(0);
+const stop = () => {
+  pause();
+  reset();
+};
+console.log('timer')
+use1Second(tick);
+return (
+  <div className="multitimerclass">
+    <div className="PlayButton" aria-label="click to play the song"> <div onClick={running ? pause : start}> <div className="v54_101"></div><button className="v54_100"></button></div> 
+    </div>    
+  <div className="greenRectangle"> {seconds} seconds </div>
+  </div>
+)
 };
 
 // const Timer = () => {
