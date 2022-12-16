e3e8f70ae8c2298dc58542bb0fa31bf38dfda35f
Ayush <ayush_gupta@brown.edu>
2022-12-15 21:17:08 -0500

    Added implementation for timer

--- frontend/src/timer.js
+++ frontend/src/timer.js
@@ -1,5 +1,6 @@
 import { useCallback, useState } from "react";
 import { use1Second } from "./useSeconds";
+import playAudio from './AudioPlayer';
 
 export const Timer = (
     {
@@ -13,8 +14,14 @@ export const Timer = (
     () => (running ? setSeconds((seconds) => seconds + 1) : undefined),
     [running]
   );
-  const start = () => setRunning(true);
-  const pause = () => setRunning(false);
+  const start = () => {
+    setRunning(true);
+    playAudio(`https://p.scdn.co/mp3-preview/4bcf32c781f2c3fa99cf363a5226ada30aff9c83?cid=fbf528e0063e4820b4fd570f750f297d`, 4)
+  }
+  const pause = () => {
+    setRunning(false);
+    playAudio(`https://p.scdn.co/mp3-preview/4bcf32c781f2c3fa99cf363a5226ada30aff9c83?cid=fbf528e0063e4820b4fd570f750f297d`, 4)
+  }
   const reset = () => setSeconds(0);
   const stop = () => {
     pause();
